74c724bdc4b849d7bead025466afc2b7207fa8a3
Steve <steveserbins@gmail.com>
2018-03-13 16:40:42 -0600

    Test push for oasis pros mobiel layout test

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+/* Show & Hide  Mobile Nav */
+
+    $("#show").click(function(){
+        $(".mobile-nav").slideDown(300);
+    });
+
+    $("#hide").click(function(){
+        $(".mobile-nav").slideUp(300);
+    });
