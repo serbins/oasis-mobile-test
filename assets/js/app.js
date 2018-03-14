8e124c57766438db740c786dd7a962e9c9e4a1eb
Steve <steveserbins@gmail.com>
2018-03-13 18:48:26 -0600

    navigation updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,11 @@
     $("#hide").click(function(){
         $(".mobile-nav").slideUp(300);
     });
+
+/* Show & Hide sub nav  */
+
+    $("#subTog").click(function(){
+        $(".sub-nav").toggle(300);
+    });
+
+  
